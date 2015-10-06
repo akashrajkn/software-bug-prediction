f015b4449e3c1ba651e134f52bf26d72780abd48
akash-raj <akashrajkn@gmail.com>
2015-10-06 23:27:37 +0530

    step1

--- data-informed-pbcm.r
+++ data-informed-pbcm.r
@@ -28,3 +28,27 @@ eclipse_20_nonnull_rows = eclipse_20_required_columns[eclipse_20_required_column
 #replicate(10,sample(x,length(x),replace = TRUE))
 ##
 
+statFunction = function(d, i)
+{
+  d2 = d[i,]
+  return (d2$pre)
+}
+
+npbootstrap = boot(eclipse_20_required_columns, statFunction, R=2)
+values = boot.array(npbootstrap, indices = T)
+
+generated_data <- values[1,]
+
+#sampling with replacement
+for(i in 1:ncol(values))
+{
+	generated_data[i] <- eclipse_20_required_columns[values[2,i], 2]
+}
+
+generated_data = sort(generated_data, decreasing = T)
+
+
+#step 2: Fit model A and model B into generated samples resulting in MLE parameter vectors 
+
+
+
