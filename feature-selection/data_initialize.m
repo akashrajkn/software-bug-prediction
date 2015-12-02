f768ff582145f6e8fce88cad463c872dc3fce3d8
akashrajkn <akashrajkn@gmail.com>
2015-12-02 22:44:10 +0530

    vectorized for faster operations

--- feature-selection/data_initialize.m
+++ feature-selection/data_initialize.m
@@ -3,13 +3,26 @@
 % features array contains all the features
 % rawdata_CM1 matrix contains the 
 
-data_CM1 = {};
-data_CM1_train = {};
-data_CM1_validate = {};
+%clear all
+
+rawdata_CM1 = read_mixed_csv('CM1.csv', ',');
+
+global data_CM1;
+global data_CM1_train;
+global data_CM1_validate;
+global size_CM_train;
+global size_CM_validate;
+data_CM1 = cell(size(rawdata_CM1,1)-1, size(rawdata_CM1,2));
 
 for i=2:1:size(rawdata_CM1,1)
     for j=1:1:size(rawdata_CM1,2)
         data_CM1{i-1,j} = rawdata_CM1{i,j};
+    end
+end
+
+for i=1:1:size(data_CM1,1)
+    for j=1:1:(size(data_CM1,2)-1)
+        data_CM1{i,j} = str2double(data_CM1{i,j});
     end
 end
 
@@ -23,18 +36,13 @@ for i=1:1:size(data_CM1,1)
     end
 end
 
+data_CM1_vector = cell2mat(data_CM1);
+
 trainDataSize = ceil(size(data_CM1, 1)*0.7);
 
-for i=1:1:trainDataSize+1
-    for j=1:1:size(data_CM1,2)
-        data_CM1_train{i,j} = data_CM1{i,j};
-    end
-end
+data_CM1_train = data_CM1_vector(1:trainDataSize, :);
+data_CM1_validate = data_CM1_vector(trainDataSize+1:end, :);
 
-for i=trainDataSize:1:size(data_CM1,1)
-    for j=1:1:size(data_CM1,2)
-        data_CM1_validate{i-trainDataSize+1,j} = data_CM1{i,j};
-    end
-end
-    
+size_CM_train = size(data_CM1_train);
+size_CM_validate = size(data_CM1_validate);
 
