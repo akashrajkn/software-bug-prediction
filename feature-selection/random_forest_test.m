5c523125ed484f8c3628aaf6c2cf85957dbfc856
akashrajkn <akashrajkn@gmail.com>
2015-12-02 22:42:50 +0530

    made changes_working

--- feature-selection/random_forest_test.m
+++ feature-selection/random_forest_test.m
@@ -9,11 +9,11 @@ for i=1:1:size(data_CM1_train,1)
 end
 
 for i=1:1:size(data_CM1_train,1)
-    classLabels(i,1) = data_CM1_train{i,38};
+    classLabels(i,1) = data_CM1_train{i,size(data_CM1_train,2)};
 end
 
 
-numTrees = 100;
+numTrees = 1000;
 
 B = TreeBagger(numTrees, features_CM1, classLabels, 'Method', 'classification');
 
@@ -21,17 +21,20 @@ newData1 = [];
 
 for i=1:1:size(data_CM1_validate,1)
     
-    for j=1:1:(size(data_CM1_train,2)-1)
+    for j=1:1:(size(data_CM1,2)-1)
         newData1(i,j) = data_CM1_validate{i,j};
     end
 
 end
 
-predChar1 = B.predict(newData1);
+%predChar1 = B.predict(newData1);
+[label, score] = predict(B, newData1);
+
 %predictedClass = str2double(predChar1);
+% 
+% [yfit, scores] = B.predict(newData1);
+% 
 
-[yfit, scores] = B.predict(newData1);
-
-[x,y,t,auc] = perfcurve(yfit, scores(:,:), '1')
+[x,y,t,auc] = perfcurve(label, score(:,2), '1');
 
 
